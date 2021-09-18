982be4ddd1464dcbf2309ecff56b9a5b8f265091
Sean <65601060+Sean-404@users.noreply.github.com>
2021-09-18 19:31:43 +0100

    Delete eDepotSystem directory

--- eDepotSystem/eDepotSystem/src/Sys/Entry.java
+++
@@ -1,8 +0,0 @@
-package Sys;
-
-public class Entry {
-	
-	public static void main(String[] args) throws Exception {
-		new DepotSystem().entryMenu();
-	}
-}
--- eDepotSystem/eDepotSystem/src/eDepotSystem/Manager.java
+++
@@ -1,9 +0,0 @@
-package eDepotSystem;
-
-public class Manager extends Driver {
-	
-	public Manager(String username, String password) {
-		super(username, password);
-	}
-
-}
